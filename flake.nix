121080c35cd1267343af12c53bb039f7e7598998
itsAnian <anian.seidl@gmail.com>
2025-02-04 22:56:56 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+        inherit system;
+        config = { allowUnfree = true; };
+    };
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+    nixos = lib.nixosSystem {
+        inherit system pkgs;
+        modules = [
+          ./configuration.nix
+	  ./hardware-configuration.nix
+	  /*home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.marc = import ./home.nix;
+          }*/
+        ];
+      };
+    };
+  };
+}
