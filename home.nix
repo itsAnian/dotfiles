6d097505eff1cad8d97820969bbed7154506cfd4
itsAnian <anian.seidl@gmail.com>
2025-02-05 22:26:40 +0100

    Add bitwarden extension for brave

--- home.nix
+++ home.nix
@@ -1,27 +1,39 @@
 { config, pkgs, ... }: {
-    home.packages = with pkgs; [ 
-      brave
-      vesktop
-      whatsapp-for-linux
-      p3x-onenote
-      spotify
-      vscode
-      kitty
-      starship
-      bitwarden
+  home.packages = with pkgs; [
+    vesktop
+    whatsapp-for-linux
+    p3x-onenote
+    spotify
+    vscode
+    kitty
+    starship
+    bitwarden
+    brave
+  ];
+
+  programs.brave = {
+    enable = true;
+    extensions = [
+      "nngceckbapebfimnlniiiahkandclblb" # bitwarden
     ];
-    home.username = "anian";
-    home.homeDirectory = "/home/anian";
-    programs.home-manager.enable = true;
-    #manual.manpages.enable = false;
-    programs.bash.enable = true;
-    home.stateVersion = "24.05";
-    programs.git = {
-    	enable = true;
-	userEmail = "anian.seidl@gmail.com";
-	userName = "itsAnian";
-	extraConfig = {
-		push.autoSetupRemote = true;
-	};
+  };
+
+  home.username = "anian";
+  home.homeDirectory = "/home/anian";
+
+  programs.home-manager.enable = true;
+  #manual.manpages.enable = false;
+  programs.bash.enable = true;
+
+  home.stateVersion = "24.05";
+
+  programs.git = {
+    enable = true;
+    userEmail = "anian.seidl@gmail.com";
+    userName = "itsAnian";
+    extraConfig = {
+      push.autoSetupRemote = true;
     };
-  }
+  };
+}
+
