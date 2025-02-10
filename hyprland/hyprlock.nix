8a13b1ad93e7df4945807b15514b3e51a3044aee
itsAnian <anian.seidl@gmail.com>
2025-02-10 20:06:53 +0100

    Customize lockscreen

--- hyprland/hyprlock.nix
+++ hyprland/hyprlock.nix
@@ -1,64 +1,110 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, ... }:
 {
-  home.packages = with pkgs;[
-  ];
-
   programs.hyprlock = {
     enable = true;
     settings = {
-      general = {
-	disable_loading_bar = true;
-	grace = 300;
-	hide_cursor = true;
-	no_fade_in = false;
-      };
+      background = [{
+	path = "~/dotfiles/hyprland/wallpaper/pexels-sebastiaan9977-1480690.jpg";
+	blur_passes = 0;
+	contrast = 0.8916;
+	brightness = 0.8172;
+	vibrancy = 0.1696;
+	vibrancy_darkness = 0.0;
+      }];
 
-      background = [
-      {
-	path = "screenshot";
-	blur_passes = 3;
-	blur_size = 8;
-      }
-      ];
+      general = {
+	no_fade_in = false;
+	grace = 0;
+	disable_loading_bar = false;
+      };
 
       label = [
       {
-	text = "Hello";
-#          color = "rgba(${hexToRgb colours.text}, 1.0)";
-#          font_family = theme.fonts.default.name;
-	font_size = 64;
-	text_align = "center";
-	halign = "center";
+	text = "Welcome!";
+	color = "rgba(0, 221, 254, 1.0)";
+	font_size = 55;
+	font_family = "SF Pro Display Bold";
+	position = "140, 320";
+	halign = "left";
 	valign = "center";
-	position = "0, 160";
       }
       {
-	text = "$TIME";
-#          color = "rgba(${hexToRgb colours.subtext1}, 1.0)";
-#          font_family = theme.fonts.default.name;
-	font_size = 32;
-	text_align = "center";
-	halign = "center";
+	text = "cmd[update:1000] echo \"<span>$(date +\"%I:%M\")</span>\"";
+	color = "rgba(0, 221, 254, 1.0)";
+	font_size = 40;
+	font_family = "SF Pro Display Bold";
+	position = "240, 240";
+	halign = "left";
 	valign = "center";
-	position = "0, 75";
+      }
+      {
+	text = "cmd[update:1000] echo -e \"$(date +\"%A, %B %d\")\"";
+	color = "rgba(0, 221, 254, 1.0)";
+	font_size = 19;
+	font_family = "SF Pro Display Bold";
+	position = "200, 175";
+	halign = "left";
+	valign = "center";
+      }
+      {
+	text = "  $USER";
+	color = "rgba(0, 0, 0, 1)";
+	outline_thickness = 0;
+	font_size = 16;
+	font_family = "SF Pro Display Bold";
+	position = "275, -140";
+	halign = "left";
+	valign = "center";
       }
       ];
 
-      input-field = [
-      {
-	size = "200, 50";
-	position = "0, -80";
-	monitor = "";
+      image = [{
+	path = "~/dotfiles/hyprland/nix_icon_neonblue.png";
+	border_size = 0;
+	border_color = "rgba(255, 255, 255, .75)";
+	size = 95;
+	rounding = -1;
+	rotate = 0;
+	reload_time = -1;
+	position = "270, 25";
+	halign = "left";
+	valign = "center";
+      }];
+
+      shape = [{
+	size = "320, 55";
+	color = "rgba(255, 255, 255, .1)";
+	rounding = -1;
+	border_size = 1;
+	border_color = "rgba(0, 221, 254, 1.0)";
+	rotate = 0;
+	xray = false;
+	position = "160, -140";
+	halign = "left";
+	valign = "center";
+      }];
+
+      input-field = [{
+	size = "320, 55";
+	outline_thickness = 0;
+	dots_size = 0.2;
+	dots_spacing = 0.2;
 	dots_center = true;
+	outer_color = "rgba(0, 221, 254, 1.0)";
+	inner_color = "rgba(255, 255, 255, 0.1)";
+	font_color = "rgb(200, 200, 200)";
 	fade_on_empty = false;
-	font_color = "rgb(202, 211, 245)";
-	inner_color = "rgb(91, 96, 120)";
-	outer_color = "rgb(24, 25, 38)";
-	outline_thickness = 5;
-	shadow_passes = 2;
-	placeholder_text = "";
-      }
-      ];
+	font_family = "SF Pro Display Bold";
+	placeholder_text = "<i><span foreground=\"##ffffff99\">🔒Enter Pass</span></i>";
+	check_color = "rgba(0, 221, 254, 1.0)";
+	fail_color = "rgba(204, 34, 34, 0.5)";
+	fail_text = "<i>FAILED: <b>($ATTEMPTS)</b></i>";
+	capslock_color = "rgba(255, 0, 0, 1.0)";
+	hide_input = false;
+	position = "160, -220";
+	halign = "left";
+	valign = "center";
+      }];
     };
   };
 }
