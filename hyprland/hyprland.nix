e32fef45a85886af15fa01a3facc4369f959b758
itsAnian <anian.seidl@gmail.com>
2025-05-26 20:10:30 +0200

    Change Sizes

--- hyprland/hyprland.nix
+++ hyprland/hyprland.nix
@@ -39,7 +39,7 @@
 
       general = {
         gaps_in = 5;
-        gaps_out = 20;
+        gaps_out = 15;
         border_size = 2;
         "col.active_border" = "rgba(a7c080ff)";
         "col.inactive_border" = "rgba(4b565c99)";
