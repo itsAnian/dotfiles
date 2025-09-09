fde2f806c6e8bf664dba492e815be7d4524940df
itsAnian <anian.seidl@gmail.com>
2025-09-09 09:06:15 +0200

    fix wallpaper not being loaded

--- home/hyprland/wpaperd.nix
+++ home/hyprland/wpaperd.nix
@@ -8,7 +8,7 @@
     enable = true;
     settings = {
       eDP-1 = {
-        path = "~/dotfiles/hyprland/wallpaper/";
+        path = "~/dotfiles/home/hyprland/wallpaper/";
         duration = "5m";
         sorting = "random";
         apply-shadow = true;
