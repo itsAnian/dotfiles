1e73fba9a62c1b3b7337ad169c9ce39609d8534a
itsAnian <anian.seidl@gmail.com>
2025-09-17 16:50:29 +0200

    Add docker to pc

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -197,6 +197,9 @@
 
   programs.zsh.enable = true;
 
+  #docker
+  virtualisation.docker.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
