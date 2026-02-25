aaa5110c7b340c20c26bddbd4b5755b10a8131bc
itsAnian <anian.seidl@gmail.com>
2026-02-25 12:51:48 +0100

    Add trusted users

--- modules/settings.nix
+++ modules/settings.nix
@@ -1,5 +1,8 @@
 {
   nixpkgs.config.allowUnfree = true;
   # flakes
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"];
+    trusted-users = ["root" "anian"];
+  };
 }
