35cf8366ac3d1640b41e2188d271c46f5178d580
itsAnian <anian.seidl@gmail.com>
2026-03-11 09:36:59 +0100

    Add boot animation

--- modules/grub.nix
+++ modules/grub.nix
@@ -1,5 +1,4 @@
-{
-    
+{pkgs, ...}: {
   # Bootloader.
   boot.supportedFilesystems = ["ntfs"];
   boot.loader.efi.canTouchEfiVariables = true;
@@ -13,5 +12,26 @@
     enable = true;
     theme = "stylish";
     footer = true;
+  };
+
+  boot.kernelParams = ["quiet" "splash" "boot.shell_on_fail" "loglevel=3" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3"];
+  boot.consoleLogLevel = 0;
+  boot.initrd.verbose = false;
+
+  boot.plymouth = {
+    enable = true;
+    theme = "infinite_seal";
+    #nix-shell -p adi1090x-plymouth-themes --run "ls /nix/store/*adi1090x-plymouth-themes*/share/plymouth/themes"
+    # Black-HUD
+    # Cross-HUD
+    # Glitch
+    # Infinite Seal
+    # Pixels
+    # Square HUD
+    themePackages = [
+      (pkgs.adi1090x-plymouth-themes.override {
+        selected_themes = ["infinite_seal"];
+      })
+    ];
   };
 }
