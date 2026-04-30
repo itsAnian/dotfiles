94ed442c7268493902853bf21f1cda8f32078d53
itsAnian <anian.seidl@gmail.com>
2026-04-30 16:18:20 +0200

    Update Flake, Add swapfile to pc

--- hosts/pc/hardware-configuration.nix
+++ hosts/pc/hardware-configuration.nix
@@ -37,7 +37,13 @@
     fsType = "ntfs";
   };
 
-  swapDevices = [];
+  swapDevices = [
+    {device = "/dev/disk/by-uuid/efdb5b23-b6ba-4479-a158-4f3cdde20883";}
+    {
+      device = "/swapfile";
+      size = 32 * 1024;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
